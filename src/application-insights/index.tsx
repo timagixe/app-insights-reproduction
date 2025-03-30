73fd78a12d924383070aa4739aa9251a1c64ba71
Tima Gixe <timagixe@protonmail.com>
2025-03-30 17:27:52 +0300

    feat: setup app insights provider with error boundary

---
+++ src/application-insights/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import {
+  AppInsightsContext,
+  AppInsightsErrorBoundary,
+  ReactPlugin,
+} from "@microsoft/applicationinsights-react-js";
+
+const reactPlugin = new ReactPlugin();
+
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString: "YOUR_CONNECTION_STRING_GOES_HERE",
+    extensions: [reactPlugin],
+    enableAutoRouteTracking: true,
+  },
+});
+
+function loadAppInsights() {
+  console.log(1);
+  try {
+    appInsights.loadAppInsights();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export function AppInsightsProvider({ children }: React.PropsWithChildren) {
+  React.useEffect(loadAppInsights, []);
+
+  return (
+    <AppInsightsContext.Provider value={reactPlugin}>
+      <ErrorBoundary>{children}</ErrorBoundary>
+    </AppInsightsContext.Provider>
+  );
+}
+
+export function ErrorBoundary({ children }: React.PropsWithChildren) {
+  return (
+    <AppInsightsErrorBoundary
+      onError={() => <h1>I believe something went wrong</h1>}
+      appInsights={reactPlugin}
+    >
+      <>{children}</>
+    </AppInsightsErrorBoundary>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import {
+  AppInsightsProvider,
+  ErrorBoundary,
+} from "./application-insights/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppInsightsProvider>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </AppInsightsProvider>
+  </StrictMode>
+);
